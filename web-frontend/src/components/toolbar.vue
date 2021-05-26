229d0af0a83c6c505348ec98ab552be2fe850882
Star-register <info@star-register.de>
2021-05-26 16:54:08 +0200

    Added custom logo and ajusted logo size in toolbar. Modified name in toolbar next to logo.

--- web-frontend/src/components/toolbar.vue
+++ web-frontend/src/components/toolbar.vue
@@ -10,8 +10,8 @@
   <div id="toolbar-image">
     <v-toolbar class="transparent" dense>
       <v-app-bar-nav-icon @click="toggleNavigationDrawer"></v-app-bar-nav-icon>
-      <img class="tbtitle hidden-xs-only" id="stellarium-web-toolbar-logo" src="@/assets/images/logo.svg" width="30" height="30" alt="Stellarium Web Logo"/>
-      <span class="tbtitle hidden-sm-and-down">Star-Register<sup></sup></span>
+      <img class="tbtitle hidden-xs-only" id="stellarium-web-toolbar-logo" src="@/assets/images/logo.svg" width="60" height="60" alt="Stellarium Web Logo"/>
+      <span class="tbtitle hidden-sm-and-down">Star-Register<sup>DeinStern</sup></span>
       <v-spacer></v-spacer>
       <target-search></target-search>
       <v-spacer></v-spacer>
