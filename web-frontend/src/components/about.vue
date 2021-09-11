41aabab74788e395d719983c8cf9e5cac2b9c857
lscobe16 <54290585+lscobe16@users.noreply.github.com>
2021-09-11 15:34:31 +0200

    Add Text to About

--- web-frontend/src/components/about.vue
+++ web-frontend/src/components/about.vue
@@ -11,7 +11,11 @@
   <v-card v-if="$store.state.showAbout">
     <v-card-title><div class="text-h5">About</div></v-card-title>
     <v-card-text style="height: 600px;">
-      <p>Welcome...</p>
+      <p>
+        Welcome to the Starregister of Star-register.de. This software is an online planetarium, giving customers of Star-register.de the possibility to access their "Sterntaufe" by entering their registration number. It is based on "Stellarium Web", an online planetarium running in your web browser based on the open source Stellarium Web Engine project. Please ask questions or report bugs regarding this implementation on star-register.de.
+        The source code for the version of Stellarium-Web used on this website (Deinstern.star-register.de), including any changes made to it, can be found on our github page. Our github page also includes instructions for setting up the software by yourself. 
+        If you like this software and want to support the creators, please consider donating to the stellarium project.
+      </p>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer><v-btn text class="blue--text darken-1" @click.native="$store.state.showAbout = false">Close</v-btn>
